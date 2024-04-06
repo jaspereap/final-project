e8090a67810c5b523a24da1ba1a4f1971eb8daab
Jaspereap <jaspereap@gmail.com>
2024-04-06 23:27:54 +0800

    Test Railway deploy using GHA

--- backend/src/main/java/com/nus/iss/travlr/controllers/ItineraryController.java
+++ backend/src/main/java/com/nus/iss/travlr/controllers/ItineraryController.java
@@ -1,7 +1,6 @@
 package com.nus.iss.travlr.controllers;
 
 import java.io.StringReader;
-import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
