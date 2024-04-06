f09590be4a194668834c9827a473a56204fd97ca
Jaspereap <jaspereap@gmail.com>
2024-04-06 22:57:33 +0800

    Test GHA

--- backend/src/main/java/com/nus/iss/travlr/controllers/FlightController.java
+++ backend/src/main/java/com/nus/iss/travlr/controllers/FlightController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.maps.DirectionsApi.Response;
 import com.nus.iss.travlr.models.Flight;
 import com.nus.iss.travlr.models.DTO.MessageType;
 import com.nus.iss.travlr.models.DTO.Request.FlightRequest;
