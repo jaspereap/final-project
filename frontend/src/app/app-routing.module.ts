5beacdabea20fdbb806915a0eb23a0c9d85a2fcb
Jaspereap <jaspereap@gmail.com>
2024-04-07 14:38:33 +0800

    Added landing page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,20 +4,19 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/home/home.component';
 import { LoginComponent } from './auth/login-main/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { LogoutComponent } from './auth/logout/logout.component';
 import { ChatroomComponent } from './chat/chatroom/chatroom.component';
 import { LoginMainComponent } from './auth/login-main/login-main.component';
 import { NewTripComponent } from './main/home/new-trip/new-trip.component';
 import { TripMainComponent } from './main/trip-main/trip-main.component';
 import { authGuard } from './guards/auth.guard';
 import { canViewTripGuard } from './guards/can-view-trip.guard';
+import { LandingComponent } from './auth/landing/landing.component';
 
 const routes: Routes = [
-  {path: '', component: MainComponent},
+  {path: '', component: LandingComponent},
   {path: 'auth', canActivateChild: [],
     children: [
       {path: 'login', component: LoginMainComponent},
-      {path: 'logout', component: LogoutComponent},
       {path: 'register', component: RegisterComponent},
     ]
   },
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { MainComponent } from './main/home/home.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LogoutComponent } from './auth/logout/logout.component';
 import { MessageService } from './shared/services/message.service';
 import { RxStompService } from './rx-stomp/rx-stomp.service';
 import { rxStompServiceFactory } from './rx-stomp/rx-stomp-service-factory';
@@ -31,14 +30,12 @@ import { FlightComponent } from './main/trip-main/flight/flight.component';
 import { DayComponent } from './main/trip-main/itinerary/day/day.component';
 import { PlaceComponent } from './main/trip-main/itinerary/day/place/place.component';
 import { TripCardComponent } from './main/home/trip-summary/trip-card/trip-card.component';
-// import { MatGoogleMapsAutocompleteModule } from '@angular-material-extensions/google-maps-autocomplete';
 import { FlexLayoutModule } from '@angular/flex-layout';
-// import { MtxCalendar } from '@ng-matero/extensions/datetimepicker';
-// import { provideNativeDatetimeAdapter } from '@ng-matero/extensions/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { TripNotificationService } from './main/trip-main/trip-notification.service';
 import { FlightDialogComponent } from './main/trip-main/flight/flight-dialog/flight-dialog.component';
 import { LodgingDialogComponent } from './main/trip-main/lodging/lodging-dialog/lodging-dialog.component';
+import { LandingComponent } from './auth/landing/landing.component';
 
 @NgModule({
   declarations: [
@@ -46,7 +43,6 @@ import { LodgingDialogComponent } from './main/trip-main/lodging/lodging-dialog/
     LoginComponent,
     MainComponent,
     RegisterComponent,
-    LogoutComponent,
     ChatroomComponent,
     LoginMainComponent,
     NavbarComponent,
@@ -61,7 +57,8 @@ import { LodgingDialogComponent } from './main/trip-main/lodging/lodging-dialog/
     PlaceComponent,
     TripCardComponent,
     FlightDialogComponent,
-    LodgingDialogComponent
+    LodgingDialogComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
@@ -74,14 +71,7 @@ import { LodgingDialogComponent } from './main/trip-main/lodging/lodging-dialog/
     MaterialModule,
     PrimengModule,
     MatNativeDateModule,
-    // MatGoogleMapsAutocompleteModule.forRoot('AIzaSyA2prL195ajVFU_yddXDahSG76X4WA3AoI'),
     FlexLayoutModule,
-    // ServiceWorkerModule.register('ngsw-worker.js', {
-    //   enabled: !isDevMode(),
-    //   // Register the ServiceWorker as soon as the application is stable
-    //   // or after 30 seconds (whichever comes first).
-    //   registrationStrategy: 'registerWhenStable:30000'
-    // }),
   ],
   providers: [
     MessageService,
